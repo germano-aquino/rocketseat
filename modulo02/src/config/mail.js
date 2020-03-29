8b9eba4a4e0064df4049805ffab52b3f37bef25a
Germano Aquino <germano.r.aquino@gmail.com>
2020-03-29 16:43:49 -0300

    Back-end da app

--- modulo02/src/config/mail.js
+++ modulo02/src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '1f57aeaed76012',
-    pass: '38adb48a660b19',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe Gobarber <noreply@gobarber.com>',
